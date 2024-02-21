7c406a10f0491d25e89634ae29fe6df76fedeca6
R3D1NM <r3dinm@gmail.com>
2024-02-21 23:16:09 +0900

    set OAK server

--- server.ts
+++ server.ts
@@ -1,3 +1,20 @@
-Deno.serve((_request: Request) => {
-    return new Response("Hello, Deno!");
-});
+import {Application, Router} from "https://deno.land/x/oak/mod.ts"
+
+
+const app = new Application()
+const router = new Router()
+const port = 5000
+
+
+// Middlewares
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+// Routes
+router.get('/',(context) => { 
+    context.response.body = "Hello, Deno"
+})
+
+// Application
+console.log(`Server is listening on port: ${port}`);
+await app.listen({port})

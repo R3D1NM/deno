b8a71eae5e5fbeeb0e8107dcfff0d8d379134b53
R3D1NM <r3dinm@gmail.com>
2024-02-21 23:22:29 +0900

    Artist interface & dummy data

--- server.ts
+++ server.ts
@@ -5,15 +5,43 @@ const app = new Application()
 const router = new Router()
 const port = 5000
 
+interface Artist {
+    id:string
+    name:string
+    group:string
+}
+
+// Dummy Data
+let artists : Artist[] = [
+    {
+        id: "1",
+        name: "LSY",
+        group: "Dreamcatcher"
+    },
+    {
+        id: "2",
+        name: "KMJ",
+        group: "Dreamcatcher"
+    },
+    {
+        id: "3",
+        name: "KBR",
+        group: "Dreamcatcher"
+    }
+]
 
 // Middlewares
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 // Routes
-router.get('/',(context) => { 
+router
+    .get('/',(context) => { 
     context.response.body = "Hello, Deno"
-})
+    })
+    .get('/artists',(context) => { 
+        context.response.body = artists
+    })
 
 // Application
 console.log(`Server is listening on port: ${port}`);
